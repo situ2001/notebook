c54130985142270bcd15f99e6a119e32f0487898
situ2001 <yongcong2001@outlook.com>
2021-03-12 10:52:59 +0800

    update(site): Fri, Mar 12, 2021 10:52:58 AM

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -38,9 +38,10 @@ module.exports = {
             {
                 title: 'JavaScript',
                 children: [
-                    './javascript/读书笔记.md',
-                    './javascript/async.md',
-                    './javascript/问题记录.md'
+                    '/javascript/问题记录.md',
+                    '/javascript/常用方法',
+                    '/javascript/读书笔记.md',
+                    '/javascript/async.md',
                 ]
             },
             {
