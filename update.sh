e6c10fe89c4872fb0c21b8bcc4e6f8416bca1133
situ2001 <yongcong2001@outlook.com>
2021-04-05 23:58:45 +0800

    shell: rename

--- update.sh
+++ update.sh
@@ -1,15 +1,41 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# set date
 now=`date`
 
-# if you want to use proxy for git 
+# build
+npm run docs:build
+
+# copy dist to deploy.git
+find ./deploy.git -mindepth 1 -not -regex "^\.\/deploy.git\/\.git.*" -delete
+cp -Rf docs/.vuepress/dist/* deploy.git/
+cd deploy.git
+
+# if you are deploying to a custom domain
+echo 'note.situ2001.com' > CNAME
+
+# git init
+git add -A
+git commit -m "deploy: $now"
+
+# if you want to apply proxy to git 
 # git config http.proxy http://<PROXY_IP>:<PORT> 
 git config http.proxy http://127.0.0.1:7890
 
-git add .
-git commit -m "update(site): $now"
-git push
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:situ2001/notebook.git master:gh-pages
 
 # if you want to use http proxy
 # git config --unset http.proxy
 git config --unset http.proxy
+
+cd -
 
 read -p "Press enter to continue"
